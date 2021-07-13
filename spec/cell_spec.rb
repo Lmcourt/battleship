bb7694f0bfd5c55fd4378a9ec8f74402bb5b34c5
Karan Mehta <karanm645@gmail.com>
2021-07-13 09:41:39 -0500

    Add reduces ships health by 1

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -38,5 +38,16 @@ RSpec.describe do
     expect(cell.fired_upon?).to eq(false)
   end
 
+  xit 'is reducing ships health by one' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    cell.fire_upon
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to eq(true)
+  end
+
+
 
 end
