06d93b016f3144f951a0b8c4f73a51e6979d095f
Laura Court <laura.mcourt@gmail.com>
2021-07-14 21:32:41 -0400

    Change coordinate hash values

--- lib/board.rb
+++ lib/board.rb
@@ -7,22 +7,22 @@ class Board
 
   def cells
     @the_board = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
+      "A1" => Cell.new("A1 value"),
+      "A2" => Cell.new("A2 value"),
+      "A3" => Cell.new("A3 value"),
+      "A4" => Cell.new("A4 value"),
+      "B1" => Cell.new("B1 value"),
+      "B2" => Cell.new("B2 value"),
+      "B3" => Cell.new("B3 value"),
+      "B4" => Cell.new("B4 value"),
+      "C1" => Cell.new("C1 value"),
+      "C2" => Cell.new("C2 value"),
+      "C3" => Cell.new("C3 value"),
+      "C4" => Cell.new("C4 value"),
+      "D1" => Cell.new("D1 value"),
+      "D2" => Cell.new("D2 value"),
+      "D3" => Cell.new("D3 value"),
+      "D4" => Cell.new("D4 value")
     }
   end
 end
