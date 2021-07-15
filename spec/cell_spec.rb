30142970e82c6095046ffbdb2e9bd6dc7d693b72
Karan Mehta <karanm645@gmail.com>
2021-07-14 20:48:14 -0500

    Update spacing

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,6 +1,6 @@
 require './lib/ship'
 require './lib/cell'
-
+require 'pry'
 RSpec.describe do
   it 'exists' do
     cell = Cell.new("B4")
@@ -35,8 +35,8 @@ RSpec.describe do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
-
     expect(cell.fired_upon?).to eq(false)
+
   end
 
   it 'is reducing ships health by one' do
