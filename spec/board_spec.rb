4605da92675d5e80e4a7c1d0d97ff0032b6308be
Laura Court <laura.mcourt@gmail.com>
2021-07-16 15:15:24 -0400

    Fix merge errors

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,11 +25,11 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-<<<<<<< HEAD
+
   it 'validates placement' do
-=======
+
   xit "validates placements" do
->>>>>>> c7d844fdcca83c7b70a28be0c3aa1b7156ca3d59
+
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -38,11 +38,9 @@ RSpec.describe Board do
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
-<<<<<<< HEAD
-  it 'is consecutive' do
-=======
+
   it "validates consecutive coordinates" do
->>>>>>> c7d844fdcca83c7b70a28be0c3aa1b7156ca3d59
+
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -50,14 +48,12 @@ RSpec.describe Board do
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-<<<<<<< HEAD
+
     expect(board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
     # extra test to see if a true statement returns true
     expect(board.valid_placement?(submarine, ["A2", "A3"])).to eq(true)
 
-=======
     expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
->>>>>>> c7d844fdcca83c7b70a28be0c3aa1b7156ca3d59
   end
 end
 
