754b56bb46c7bb1642db6d69de4246d0ee8def33
Laura Court <laura.mcourt@gmail.com>
2021-07-16 13:28:32 -0400

    Update valid placement tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,4 +24,29 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("E1")).to eq(false)
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
+
+  it 'validates placement' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it 'is consecutive' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
+    # extra test to see if a true statement returns true
+    expect(board.valid_placement?(submarine, ["A2", "A3"])).to eq(true)
+
+  end
 end
+
+# diagonal either all the same number or all the same letter
