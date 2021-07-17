f186eb26625dac2e77368985c3287bf4c1d53b64
Laura Court <laura.mcourt@gmail.com>
2021-07-16 21:52:59 -0400

    Add numbers and letters methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Board
 
   attr_reader :the_board
@@ -31,49 +30,69 @@ class Board
     cells.has_key?(key)
   end
 
-  def check_row
-    horizontal_array = []
 
-  def valid_placement(ship, coordinates)
+  def valid_placement?(ship, coordinates)
     ship.length == coordinates.length
-    require "pry"; binding.pry
 
-    cells.values.each_slice do
     letters = []
-    letters << cells.values.split(0)
+    coordinates.each do |coordinate|
+      letters << coordinate[0]
+    end
+
+    letters.uniq.count <= 1
+
     numbers = []
-    numbers << cells.values.split(1)
+    coordinates.each do |coordinate|
+      numbers << coordinate[1]
+    end
+
+    numbers.uniq.count <= 1
   end
+
+    # value_arrays = []
+    # cells.values.each do |value|
+    #   value_arrays <<  value.split(/\W+/)
+    # end
+
+# make a game class
+
+# make a seperate player and computer class
+
+
+    # letters = []
+    # letters << cells.values.split(0)
+    # numbers = []
+
     # cells.keys.each_cons.each(ship.length) do |key|
     #   horizontal_array << key
     # end
-    # # horizontal_array.map do |array|
-    # #   # delete-if or reject!
-    # # end
+    # horizontal_array.map do |array|
+    #   # delete-if or reject!
+    # end
     # horizontal_array.any? do |horizontal|
     #   horizontal == coordinates
     # end
-  end
 
-# #check if num lets are the same and consecutive
-#   def check_columns
-#     require "pry"; binding.pry
-#     vertical_array = []
-#     horizontal_array.each_cons(4) do |array|
-#       vertical_array << array
-#     end
-#   end
-#
-#   def valid_placement?(ship, coordinates)
-#       # not really sure, but probably going to need to do something similar.
-#       # this is just an 'outline'
-#       # vertical_array = []
-#       # cells.keys.each_cons(ship.length) do |key|
-#       #   vertical_array << key
-#       # end
-#       # vertical_array = horizontal_array.transpose.each do |cell|
-#
-#   end
+
+#check if num lets are the same and consecutive
+  # def check_columns
+  #   require "pry"; binding.pry
+  #   vertical_array = []
+  #   horizontal_array.each_cons(4) do |array|
+  #     vertical_array << array
+  #   end
+  # end
+
+  # def valid_placement?(ship, coordinates)
+  #     # not really sure, but probably going to need to do something similar.
+  #     # this is just an 'outline'
+  #     vertical_array = []
+  #   #something.each_cons(ship.length) do |key|
+  #       vertical_array << key
+  #     end
+  #     vertical_array = horizontal_array.transpose.each do |cell|
+  #
+  # end
 end
 # split letters and numbers and check if all letters or all numbers are the same
 #check if in the same row or column
