ba69f8f6422dfbe976d9543c640e82e4a7e15a52
Laura Court <laura.mcourt@gmail.com>
2021-07-18 19:39:13 -0400

    Initialize and add ships array

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,16 @@
 class Computer
 
+  attr_reader :ships
   def initialize
+    @ships = []
+  end
 
+  def computer_ships
+    @ships << Ship.new("Cruiser", 3)
+    @ships << Ship.new("Submarine", 2)
   end
 
   #display board
   # computer shots
-
 
 end
