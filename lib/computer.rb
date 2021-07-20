2f62d0bb9241c1f842c9e4e38a4f75a369fb0b3a
Laura Court <laura.mcourt@gmail.com>
2021-07-20 14:53:05 -0400

    Change variable names

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,6 +3,7 @@ require './lib/ship'
 require './lib/cell'
 
 
+
 class Computer
 
   attr_reader :ships, :computer_board, :computer_fires, :comp_render_and_report
@@ -10,8 +11,8 @@ class Computer
     @ships = [Ship.new("Submarine", 2), Ship.new("Cruiser", 3)]
     @computer_board = computer_board
     @player_board = player_board
-    @comp_render_and_report = comp_render_and_report
-    
+    # @comp_render_and_report = comp_render_and_report(player_board)
+
   end
 
   def computer_placement
@@ -25,22 +26,22 @@ class Computer
     end
   end
 
-  def select_coordinate(player)
-    player.player_board.cells.keys.sample
+  def select_coordinate(player_board)
+    player_board.cells.keys.sample
   end
 
-  def computer_fires(player)
-   player.player_board.cells[select_coordinate].fire_upon
+  def computer_fires(player_board)
+   player_board.cells[select_coordinate].fire_upon
    select_coordinate
   end
 
-  require "pry"; binding.pry
-  def comp_render_and_report(player)
-    if player.player_board.cells[computer_fires(player)].render == "X"
+  def comp_render_and_report(player_board)
+    require "pry"; binding.pry
+    if player_board.cells[computer_fires(player_board)].render == "X"
       puts "I shot at #{select_coordinate} GET SUNK."
-    elsif player.player_board.cells[computer_fires(player)].render == "H"
+    elsif player_board.cells[computer_fires(player_board)].render == "H"
       puts "I shot at #{select_coordinate} and it's a hit. I'm gonna win."
-    elsif player.player_board.cells[computer_fires(player)].render == "M"
+    elsif player_board.cells[computer_fires(player_board)].render == "M"
       puts "I missed but I'm not throwing away my SHOT."
     end
   end
