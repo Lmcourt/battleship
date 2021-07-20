edf256a09ddd51e1759082b035a604af1bf529c6
Laura Court <laura.mcourt@gmail.com>
2021-07-20 14:07:18 -0400

    Updatte render and report

--- lib/computer.rb
+++ lib/computer.rb
@@ -11,6 +11,7 @@ class Computer
     @computer_board = computer_board
     @player_board = player_board
     @comp_render_and_report = comp_render_and_report
+    
   end
 
   def computer_placement
@@ -19,27 +20,28 @@ class Computer
       until @computer_board.valid_placement?(ship, coordinates) == true
         coordinates = @computer_board.cells.keys.sample(ship.length)
       end
-      @computer_board.place(ship, coordinates)
-      @computer_board.place(ship, coordinates)
+      @computer_board.place(ship, @comp_coordinates)
+      @computer_board.place(ship, @comp_coordinates)
     end
   end
 
-  def select_coordinate
-    @player_board.cells.keys.sample
+  def select_coordinate(player)
+    player.player_board.cells.keys.sample
   end
 
-  def computer_fires
-   @player_board.cells[select_coordinate].fire_upon
+  def computer_fires(player)
+   player.player_board.cells[select_coordinate].fire_upon
    select_coordinate
   end
 
-  def comp_render_and_report
-    if @player_board.cells[computer_fires].render == "M"
-      puts "I missed but I'm not throwing away my SHOT."
-    elsif @player_board.cells[computer_fires].render == "H"
-      puts "I shot at #{select_coordinate} and it's a hit. I'm gonna win."
-    else @player_board.cells[computer_fires].render == "X"
+  require "pry"; binding.pry
+  def comp_render_and_report(player)
+    if player.player_board.cells[computer_fires(player)].render == "X"
       puts "I shot at #{select_coordinate} GET SUNK."
+    elsif player.player_board.cells[computer_fires(player)].render == "H"
+      puts "I shot at #{select_coordinate} and it's a hit. I'm gonna win."
+    elsif player.player_board.cells[computer_fires(player)].render == "M"
+      puts "I missed but I'm not throwing away my SHOT."
     end
   end
 end
