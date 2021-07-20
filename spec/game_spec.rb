b102ab546e4c1fe25860d24b4ebcb39c05ce6f65
Karan Mehta <karanm645@gmail.com>
2021-07-20 17:42:10 -0500

    Created game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,7 +11,58 @@ RSpec.describe Game do
 
   it "welcomes player" do
     game = Game.new
-
+    welcome = ""
     expect(welcome).to eq("")
+    # expect(welcome).to eq("Enter P to play. Enter Q to quit.")
   end
+
+  it "has a attributes" do
+    game = Game.new
+
+    expect(game.computer_board).to be_a(Board)
+    expect(game.player_board).to be_a(Board)
+    expect(game.computer).to be_a(Computer)
+    expect(game.player).to be_a(Player)
+  end
+
+  it "player has lost" do
+    game = Game.new
+    game.player.cruiser.hit
+    game.player.cruiser.hit
+    game.player.cruiser.hit
+
+    game.player.submarine.hit
+    game.player.submarine.hit
+    expect(game.end_game_message).to eq("You have been defeated!")
+
+  end
+  it "computer has lost" do
+    game = Game.new
+    game.computer.cruiser.hit
+    game.computer.cruiser.hit
+    game.computer.cruiser.hit
+
+    game.computer.submarine.hit
+    game.computer.submarine.hit
+    expect(game.end_game_message).to eq("Victory!")
+
+  end
+
+  it "has ended" do
+    game = Game.new
+    game.computer.cruiser.hit
+    game.computer.cruiser.hit
+    game.computer.cruiser.hit
+
+    game.computer.submarine.hit
+    game.computer.submarine.hit
+    expect(game.game_over?).to eq(true)
+
+  end
+  #game has ended test, same setup as computer has lost
 end
+# game has player board
+# game has computer board
+
+# game has computer
+# game has player
